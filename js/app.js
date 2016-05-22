144d09651e9cb54a07bf55f77bd3bc571c3affaa
Lizzie Paris <lzz.paris@gmail.com>
2016-05-22 15:02:48 -0700

    recordMove uses square id correctly

--- js/app.js
+++ js/app.js
@@ -6,12 +6,13 @@ $(document).ready(function(){
 	$(".col").click(function(){
 		if(xTurn){
 			$(this).children(".x").css("display","inline-block");
-
-			recordMove(playerX,0,0);
-			recordMove(playerX,2,0);
-			recordMove(playerX,1,1);
-			recordMove(playerX,1,2);
+			var testId= this.id;
+			var move;
+			move=testId.split("-");
+			recordMove(playerX,move[0],move[1]);
+			playerX.numTurns+=1;
 			xTurn=false;
+			if(playerX.numTurns==3){finish(playerX)};
 		}
 		else {
 			$(this).children(".o").css("display","inline-block");
@@ -32,6 +33,10 @@ var Player= {
 
 var recordMove = function(player,col,row){
 	player.scoreCard[col][row]=1;
+}
+
+var finish = function(player){
+	console.log(player.scoreCard);
 }
 
 
